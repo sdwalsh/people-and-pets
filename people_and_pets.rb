2ad5760d5bf1ccafac78c4c6825928ea2e327db0
Sean Walsh <sdwalsh@mirango.io>
2017-08-10 14:29:34 -0400

    add POST upload with functions to determine delimiter and normalize imported data

--- people_and_pets.rb
+++ people_and_pets.rb
@@ -6,4 +6,72 @@ class PeopleAndPets < Sinatra::Base
     status 200
     haml :home, :format => :html5
   end
+
+  post '/upload' do
+    if params[:csv] && params[:csv][:tempfile]
+      file = params[:csv][:tempfile]
+      delimiter = sniffer(file)
+      if !delimiter
+        return status 400
+      end
+      # array of hashes eventually can be converted to json
+      response = normalize(file, delimiter)
+      if !response
+        return status 400
+      end      
+      response.to_json
+    else
+      return status 400
+    end
+  end
+
+  # Normalize the file values into a standard format (defined in docs)
+  # important note: ordering of hash literals is guaranteed
+  # https://stackoverflow.com/questions/31418673/is-order-of-a-ruby-hash-literal-guaranteed
+  # :last :first :m :pet :birthday :color
+  # returns an array of hashes
+  def normalize(file, delimiter)
+    response = []
+    case delimiter
+      when ','
+        CSV.foreach(file, col_sep: delimiter) do |row|
+          hash = {:last => row[0], :first => row[1], :m => "None", :pet => row[2], :birthday => row[4], :color => row[3]}
+          response.push(hash)
+        end
+      when ' '
+        CSV.foreach(file, col_sep: delimiter) do |row|
+          case row[3]
+            when 'C' then row[3] = 'Cat'
+            when 'D' then row[3] = 'Dog'
+            when 'B' then row[3] = 'Both'
+            else row[3] = 'None'
+          end
+          row[4].gsub! '-', '/'
+          hash = {:last => row[0], :first => row[1], :m => row[2], :pet => row[3], :birthday => row[4], :color => row[5]}
+          response.push(hash)
+        end
+      when '|'
+        CSV.foreach(file, col_sep: delimiter) do |row|
+          row[5].gsub! '-', '/'
+          hash = {:last => row[0], :first => row[1], :m => row[2], :pet => row[3], :birthday => row[5], :color => row[4]}
+          response.push(hash)
+        end
+      else
+        return nil
+    end
+    return response
+  end
+
+  # Sniffer returns the delimiter in use
+  # for more robust searching consider using a sniffer library or creating a sniffer class
+  # this sniffer function meets all requirements of project guidelines
+  def sniffer(file)
+    line = File.foreach(file).first
+    line.each_char do |c|
+      if c == '|' || c == ' ' || c == ','
+        return c
+      end
+    end
+    return nil
+  end
 end
