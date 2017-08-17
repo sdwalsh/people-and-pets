96e32cedc14e4f0a0d3a92cdace7799b7ce8b1bb
Sean Walsh <sdwalsh@mirango.io>
2017-08-17 05:56:53 -0400

    update test helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'capybara/rspec'
 require 'selenium-webdriver'
 require_relative '../people_and_pets'
 
+Capybara.app = PeopleAndPets
 Capybara.server = :puma
 Capybara.javascript_driver = :selenium
-Capybara.app = PeopleAndPets
+Capybara.default_wait_time = 10
