0358feb44f20b20917d56bc36850de69b8eec18b
Sean Walsh <sdwalsh@mirango.io>
2017-08-17 06:18:02 -0400

    add ruby version to gemfile and update .rspec

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org' do
+  ruby "2.4.1"
   gem 'sinatra'
   gem 'puma'
   gem 'haml'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,4 @@ require_relative '../people_and_pets'
 Capybara.app = PeopleAndPets
 Capybara.server = :puma
 Capybara.javascript_driver = :selenium
-Capybara.default_wait_time = 10
+Capybara.default_max_wait_time = 10
